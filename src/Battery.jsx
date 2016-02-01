39c16f47f5c51b74accbeacd8b5ecb105a514231
hwclass <harderworking.class@gmail.com>
2016-02-02 00:35:20 +0200

    developed the battery component

---
+++ src/Battery.jsx
@@ -0,0 +1,38 @@
+/*!
+ * React Battery. React component for tracking the battery status of your computer or mobile device.
+ *
+ * Copyright (c) 2016 Barış Güler
+ * http://hwclass.in
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ *
+ * Launch  : July 2016
+ * Version : 1.0.0
+ * Released: February 2nd, 2016
+ */
+
+import React from 'react'
+
+export default class Battery extends React.Component {
+
+	constructor (props) {
+		super(props)
+		const battery  = navigator.battery,
+    			level    = battery.level * 100
+		this.state = {
+			level : level
+		}
+	}
+
+	render () {
+    props = {...props}
+    props.level = this.state.level
+    return (
+    	<span {...props}></span>
+    )
+	}
+
+}
+
